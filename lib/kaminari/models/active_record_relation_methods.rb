d65306e4d62e42095121c80d11c9447a78c662bc
bartes <bartosz.knapik@llp.pl>
2011-04-07 16:16:45 +0200

    fix for rails 3.0.6

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -3,15 +3,18 @@ module Kaminari
     extend ActiveSupport::Concern
     module InstanceMethods
       def total_count #:nodoc:
-        c = except(:offset, :limit).count
+        c = except(:offset, :limit).count(nil, {:dont_use_kaminari => true})
         # .group returns an OrderdHash that responds to #count
         c.respond_to?(:count) ? c.count : c
       end
+
       def size
         [[0,(total_count - (current_page - 1) * limit_value)].max, limit_value].min
       end
-      def count
-        size
+
+      def count(column_name = nil, options = {})
+        dont_use_kaminari = options.delete(:dont_use_kaminari)
+        dont_use_kaminari ? super : size
       end
     end
   end
