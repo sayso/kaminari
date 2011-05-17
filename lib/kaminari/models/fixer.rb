3b6c0a1869005431b3d9b67d3ddcb4209f688ad5
bartes <bartosz.knapik@llp.pl>
2011-05-17 09:34:14 +0200

    permission improvements for fixer

--- lib/kaminari/models/fixer.rb
+++ lib/kaminari/models/fixer.rb
@@ -2,6 +2,91 @@ module Kaminari
 
   class Fixer
 
+    private
+
+    def self.do_array(collection, options)
+      Kaminari.paginate_array(collection)
+      collection.instance_eval <<-EVAL
+        def current_page
+          @current_page || 1
+        end
+        def limit_value
+          per_page
+        end
+        def per_page
+          @per_page || size
+        end
+        def page(value)
+          @current_page = value.to_i.zero? ? 1 : value.to_i
+          self
+        end
+        def per(value)
+          @per_page = value
+          self
+        end
+        def total_count
+          #{options[:total].to_s + ' ||' if options[:total]} size
+        end
+        def num_pages
+          (total_count.to_f / (per_page.zero? ? 1 : per_page)).ceil
+        end
+        def offset
+          (current_page - 1) * per_page
+        end
+        def offset_value
+          offset
+        end
+        def scoped
+          self[offset...offset + per_page]
+        end
+        def all
+          scoped
+        end
+      EVAL
+      collection
+    end
+
+    def self.do_thinking_sphinx(collection, options)
+      collection.instance_eval <<-EVAL
+        def limit_value
+          per_page
+        end
+        def offset_value
+          offset
+        end
+        def page(value)
+          self
+        end
+        def per(value)
+          self
+        end
+        def total_count
+          total_entries
+        end
+        def scoped
+          self
+        end
+        def num_pages
+          total_pages
+        end
+        def all
+          scoped
+        end
+      EVAL
+      collection
+    end
+
+    def self.do_ar_relation(collection, options)
+      collection.instance_eval <<-EVAL
+        def total_count
+          #{options[:total]}
+        end
+      EVAL
+      collection
+    end
+
+    public
+
     def self.do(input_collection, options = {})
       collection = input_collection
       if collection.is_a?(ThinkingSphinx::Search)
@@ -12,88 +97,5 @@ module Kaminari
         do_array(collection, options)
       end
     end
-  end
-
-  private
-
-  def self.do_array(collection, options)
-    Kaminari.paginate_array(collection)
-    collection.instance_eval <<-EVAL
-      def current_page
-        @current_page || 1
-      end
-      def limit_value
-        per_page
-      end
-      def per_page
-        @per_page || size
-      end
-      def page(value)
-        @current_page = value.to_i.zero? ? 1 : value.to_i
-        self
-      end
-      def per(value)
-        @per_page = value
-        self
-      end
-      def total_count
-        #{options[:total].to_s + ' ||' if options[:total]} size
-      end
-      def num_pages
-        (total_count.to_f / (per_page.zero? ? 1 : per_page)).ceil
-      end
-      def offset
-        (current_page - 1) * per_page
-      end
-      def offset_value
-        offset
-      end
-      def scoped
-        self[offset...offset + per_page]
-      end
-      def all
-        scoped
-      end
-    EVAL
-    collection
-  end
-
-  def self.do_thinking_sphinx(collection, options)
-    collection.instance_eval <<-EVAL
-      def limit_value
-        per_page
-      end
-      def offset_value
-        offset
-      end
-      def page(value)
-        self
-      end
-      def per(value)
-        self
-      end
-      def total_count
-        total_entries
-      end
-      def scoped
-        self
-      end
-      def num_pages
-        total_pages
-      end
-      def all
-        scoped
-      end
-    EVAL
-    collection
-  end
-
-  def self.do_ar_relation(collection, options)
-    collection.instance_eval <<-EVAL
-      def total_count
-        #{options[:total]}
-      end
-    EVAL
-    collection
   end
 end
